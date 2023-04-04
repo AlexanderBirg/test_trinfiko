40f79facb853488129cc27324112a61354550233
Alexander Birg <87033042+AlexanderBirg@users.noreply.github.com>
2023-04-04 19:23:08 +0400

    update
    Added new page - Contacts and minor edits

--- src/components/complex/preloader/preloader.js
+++ src/components/complex/preloader/preloader.js
@@ -1,4 +1,9 @@
 // Everything but IE
-window.addEventListener('load', () => {
-  document.querySelector('.preloader').classList.remove('preloader--active');
-}, false);
+
+const preloader = document.querySelector('.preloader');
+
+if (preloader) {
+  window.addEventListener('load', () => {
+    document.querySelector('.preloader').classList.remove('preloader--active');
+  }, false);
+}
--- src/scripts.js
+++ src/scripts.js
@@ -9,7 +9,7 @@ import './components/embedded/form/form.js';
 // ПЛАВНАЯ ПРОКРУТКА К ЭЛЕМЕНТАМ
 const anchors = document.querySelectorAll('[data-my-anchor]');
 
-if (anchors) {
+if (anchors.length) {
   anchors.forEach((item) => {
     item.onclick = () => {
       jump(item.getAttribute('data-my-anchor'), {
@@ -67,3 +67,111 @@ window.addEventListener('scroll', () => {
   }
   showBlocks(); // Вызываем функцию для первоначального запуска
 });
+
+// ЯНДЕКС КАРТА
+/* global ymaps */
+
+if (document.querySelector('#map')) {
+  const TRINFIKO_POSITION = [55.77779714854112, 37.63736076633297];
+
+  const init = () => {
+    const myMap = new ymaps.Map('map', {
+      center: TRINFIKO_POSITION,
+      zoom: 15,
+      controls: [],
+    });
+
+    // Создадим пользовательский макет ползунка масштаба.
+    const ZoomLayout = ymaps.templateLayoutFactory.createClass(`
+      <div class="ya-map__buttons">
+        <button id='zoom-in' class='ya-map__button ya-map__button--in'><span>+</span></button>
+        <button id='zoom-out' class='ya-map__button ya-map__button--out'><span>-</span></button>
+      </div>`, {
+      // Переопределяем методы макета, чтобы выполнять дополнительные действия
+      // при построении и очистке макета.
+      build() {
+        // Вызываем родительский метод build.
+        ZoomLayout.superclass.build.call(this);
+        // Привязываем функции-обработчики к контексту и сохраняем ссылки
+        // на них, чтобы потом отписаться от событий.
+        this.zoomInCallback = ymaps.util.bind(this.zoomIn, this);
+        this.zoomOutCallback = ymaps.util.bind(this.zoomOut, this);
+        // Начинаем слушать клики на кнопках макета.
+        document.getElementById('zoom-in').addEventListener('click', this.zoomInCallback);
+        document.getElementById('zoom-out').addEventListener('click', this.zoomOutCallback);
+      },
+
+      clear() {
+        // Снимаем обработчики кликов.
+        document.getElementById('zoom-in').removeEventListener('click', this.zoomInCallback);
+        document.getElementById('zoom-out').removeEventListener('click', this.zoomOutCallback);
+
+        // Вызываем родительский метод clear.
+        ZoomLayout.superclass.clear.call(this);
+      },
+
+      zoomIn() {
+        const map = this.getData().control.getMap();
+        map.setZoom(map.getZoom() + 1, { checkZoomRange: true });
+      },
+
+      zoomOut() {
+        const map = this.getData().control.getMap();
+        map.setZoom(map.getZoom() - 1, { checkZoomRange: true });
+      },
+    });
+    const zoomControl = new ymaps.control.ZoomControl({ options: { layout: ZoomLayout } });
+
+    // расположение кнопок масштабирования в зависимости от размера видимой области
+    let yaTop = 113;
+    let yaRight = 5;
+
+    if (window.innerWidth > 767) {
+      yaTop = 225;
+      yaRight = 22;
+    }
+    if (window.innerWidth > 1279) {
+      yaTop = 222;
+      yaRight = 29;
+    }
+
+    myMap.controls.add(zoomControl, {
+      float: 'none',
+      position: {
+        top: yaTop,
+        right: yaRight,
+      },
+    });
+
+    const MyIconContentLayout = ymaps.templateLayoutFactory.createClass(`
+    <div class="trinfiko-round-icon">
+      <div class="trinfiko-round-icon__wrapper">
+        <svg style="fill: white;" class="inline-svg">
+          <use xlink:href="images/sprite.svg#trinfiko"></use>
+        </svg>
+      </div>
+    </div>
+    `);
+
+    const myPlacemarkWithContent = new ymaps.Placemark(TRINFIKO_POSITION, {}, {
+      // Опции.
+      // Необходимо указать данный тип макета.
+      iconLayout: 'default#imageWithContent',
+      // Своё изображение иконки метки.
+      iconImageHref: '',
+      // Размеры метки.
+      iconImageSize: [48, 48],
+      // Смещение левого верхнего угла иконки относительно
+      // её "ножки" (точки привязки).
+      iconImageOffset: [-24, -24],
+      // Смещение слоя с содержимым относительно слоя с картинкой.
+      iconContentOffset: [15, 15],
+      // Макет содержимого.
+      iconContentLayout: MyIconContentLayout,
+    });
+
+    myMap.geoObjects.add(myPlacemarkWithContent);
+  };
+
+  ymaps.ready(init);
+}
