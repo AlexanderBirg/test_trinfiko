ffec2c26385625747187293545d26d86c619ee46
Alexander Birg <87033042+AlexanderBirg@users.noreply.github.com>
2023-04-12 00:27:30 +0400

    update

--- src/components/embedded/accordion/accordion.js
+++ src/components/embedded/accordion/accordion.js
@@ -1,5 +1,12 @@
 const accordions = document.querySelectorAll('.accordion');
 
+function closeItem(item) {
+  const body = item.querySelector('.accordion__body');
+  body.style.maxHeight = null;
+
+  item.classList.remove('accordion__item--active');
+}
+
 if (accordions.length) {
   accordions.forEach((ac) => {
     ac.querySelectorAll('.accordion__item').forEach((item) => {
@@ -7,12 +14,19 @@ if (accordions.length) {
       const body = item.querySelector('.accordion__body');
 
       header.addEventListener('click', () => {
-        item.classList.toggle('accordion__item--active');
+        // если указана настройка
+        if (ac.getAttribute('data-accordion-single')) {
+          // закрытие всех спойлеров
+          ac.querySelectorAll('.accordion__item').forEach((el) => {
+            closeItem(el);
+          });
+        }
 
-        if (item.classList.contains('accordion__item--active')) {
-          const maxHeight = body.scrollHeight + body.style.paddingTop + body.style.paddingBottom;
-          body.style.maxHeight = `${maxHeight}px`;
+        if (!item.classList.contains('accordion__item--active')) {
+          item.classList.add('accordion__item--active');
+          body.style.maxHeight = `${body.scrollHeight}px`;
         } else {
+          item.classList.remove('accordion__item--active');
           body.style.maxHeight = null;
         }
       });
